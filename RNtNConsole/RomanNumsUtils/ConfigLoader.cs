d34e84542368bf2498460816ed317c9e526110bb
Clark <daprogrammer1@gmail.com>
2022-08-30 11:41:36 +0800

    add minimalist config loader and apply it soon

---
+++ RNtNConsole/RomanNumsUtils/ConfigLoader.cs
@@ -0,0 +1,41 @@
+namespace RomanNumsUtils;
+
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using static System.IO.File;
+
+class Config
+{
+	public bool ViceVersa = false;
+}
+
+public sealed class ConfigLoader 
+{
+	private bool? _IsViceVersa;
+	public bool IsViceVersa
+	{
+		get
+		{
+			if (_IsViceVersa == null) throw new Exception("You haven't load the config file!");
+			else return _IsViceVersa ?? false;
+		}
+		set
+		{
+			_IsViceVersa = value;
+		}
+	} 
+	public void Load(string yamlPath)
+	{
+		string yml = ReadAllText(yamlPath);
+		IDeserializer deserializer = new DeserializerBuilder()
+			.WithNamingConvention(PascalCaseNamingConvention.Instance)
+			.Build();
+		try {
+			var p = deserializer.Deserialize<Config>(yml);
+			_IsViceVersa = p.ViceVersa;
+		} catch (YamlException ex) {
+			Console.WriteLine(ex.Message);
+		}		
+	}
+}
